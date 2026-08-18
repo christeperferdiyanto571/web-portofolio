52ec15c95a2ac5c58d4bea11e5cd809a86e98117
Chris Teper Ferdiyanto <christeperferdiyanto8362@gmail.com>
2026-08-18 13:31:19 +0700

    Fix Vite hashing issue for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,13 @@ export default defineConfig({
             ignored: ['**/storage/framework/views/**'],
         },
     },
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]'
+            }
+        }
+    }
 });
